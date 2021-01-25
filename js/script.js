97e9f96f86d5a48618c7b129338c274c2fdf735c
tpo-dburbine <77117608+tpo-dburbine@users.noreply.github.com>
2021-01-25 10:15:38 -0500

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
+/* global data */
 /*
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
-
-
 
 /*
 For assistance:
@@ -11,75 +10,70 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
-
-
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
-
-//global variables
-//const headerHTML = document.querySelector('.header')
+// global variables
+// const headerHTML = document.querySelector('.header')
 const studentListUL = document.querySelector('ul.student-list')
 const linkList = document.querySelector('ul.link-list')
 const itemsPerPage = 9
 let activeButtonIndex = 0
 
-//adding the search bar to html
+// adding the search bar to html
 const header = document.querySelector('header')
 let searchBarHTML =
-   `<label for="search" class="student-search">
+`<label for="search" class="student-search">
                   <input id="search" placeholder="search by name...">
                   <button type="button"><img src="img/icn-search.svg" alt="Search Icon"></button>
                </label> `
 header.insertAdjacentHTML('beforeend', searchBarHTML)
 
-//-----------------------------------------------------------------------------
-//FUNCTIONS
-//-----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// FUNCTIONS
+// -----------------------------------------------------------------------------
 
+function filterSearch (list) {
+  // initialize empty string
+  let searchStudents = []
+  // create var 'input' to select ID search
+  const input = document.querySelector('#search').value.toLowerCase()
 
-function filterSearch(list) {
-   //initialize empty string
-   let searchStudents = []
-   //create var 'input' to select ID search
-   const input = document.querySelector('#search').value.toLowerCase()
+  // for loop to loop through data array
+  for (let i = 0; i < list.length; i++) {
+    // variable set to equal title, firstname, and lastname in data array
+    let title = list[i].name.title
+    let firstName = list[i].name.first
+    let lastName = list[i].name.last
+    // create string literal to combine variables and set equal to studentName var
+    let studentName = `${title} ${firstName} ${lastName}`.toLowerCase()
 
-   //for loop to loop through data array
-   for (let i = 0; i < list.length; i++) {
-      //variable set to equal title, firstname, and lastname in data array
-      let title = list[i].name.title
-      let firstName = list[i].name.first
-      let lastName = list[i].name.last
-      //create string literal to combine variables and set equal to studentName var
-      let studentName = `${title} ${firstName} ${lastName}`.toLowerCase()
-
-      //conditional to check if full studentName var includes user input
-      //push names that include user input from data array to empty SearchStudents array
-      if (studentName.includes(input)) {
-         searchStudents.push(list[i])
-      }
-      //function calls to display page and add pagination
-      showPage(searchStudents, 1)
-      addPagination(searchStudents)
-   }
+    // conditional to check if full studentName var includes user input
+    // push names that include user input from data array to empty SearchStudents array
+    if (studentName.includes(input)) {
+      searchStudents.push(list[i])
+    }
+    // function calls to display page and add pagination
+    showPage(searchStudents, 1)
+    addPagination(searchStudents)
+  }
 }
 
+// showPage function used to add list of students to page
+function showPage (list, page) {
+  const startIndex = (page - 1) * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
+  studentListUL.innerHTML = ''
+  let studentHTML
 
-//showPage function used to add list of students to page
-function showPage(list, page) {
-   const startIndex = (page - 1) * itemsPerPage
-   const endIndex = startIndex + itemsPerPage
-   studentListUL.innerHTML = ''
-   let studentHTML
-
-   if (list.length > 0) {
-      //for loop to go through data array and nested conditional to display students within specified index range
-      for (let i = 0; i < list.length; i++) {
-         if (i >= startIndex && i < endIndex) {
-            studentHTML =
-               `<li class="student-item cf">
+  if (list.length > 0) {
+    // for loop to go through data array and nested conditional to display students within specified index range
+    for (let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+        studentHTML =
+          `<li class="student-item cf">
                         <div class="student-details">
                            <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
                            <h3>${list[i].name.title}. ${list[i].name.first} ${list[i].name.last}</h3>
@@ -89,71 +83,62 @@ function showPage(list, page) {
                            <span class="date">Joined ${list[i].registered.date}</span>
                         </div>
                      </li>`
-            studentListUL.insertAdjacentHTML('beforeend', studentHTML)
-         }
+        studentListUL.insertAdjacentHTML('beforeend', studentHTML)
       }
-   } else {
-      studentHTML = `<li>No results for your search were found. Try again!</li>`
-      studentListUL.insertAdjacentHTML('beforeend', studentHTML)
-   }
+    }
+  } else {
+    studentHTML = `<li>No results for your search were found. Try again!</li>`
+    studentListUL.insertAdjacentHTML('beforeend', studentHTML)
+  }
 }
-
-
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
-function addPagination(list) {
-   //find number of pages needed
-   let pages = Math.ceil(list.length / itemsPerPage)
-   //set linklist to empty string
-   linkList.innerHTML = ''
-   //initialize buttons variable
-   let buttons
+function addPagination (list) {
+  // find number of pages needed
+  let pages = Math.ceil(list.length / itemsPerPage)
+  // set linklist to empty string
+  linkList.innerHTML = ''
+  // initialize buttons variable
+  let buttons
 
-   //loop similar to showPage to add buttons to HTML
-   for (let i = 1; i <= pages; i++) {
-      buttons = `
+  // loop similar to showPage to add buttons to HTML
+  for (let i = 1; i <= pages; i++) {
+    buttons = `
          <li>
             <button type="button">${i}</button>
          </li>`
-      linkList.insertAdjacentHTML('beforeend', buttons)
-   }
+    linkList.insertAdjacentHTML('beforeend', buttons)
+  }
 }
 
+// -----------------------------------------------------------------------------
+// EVENT LISTENERS
+// -----------------------------------------------------------------------------
 
-
-//-----------------------------------------------------------------------------
-//EVENT LISTENERS
-//-----------------------------------------------------------------------------
-
-
-//event listener for 'keyup'
+// event listener for 'keyup'
 header.addEventListener('keyup', () => {
-   filterSearch(data)
+  filterSearch(data)
 })
 
-
-//event listener for clicking pagination buttons
+// event listener for clicking pagination buttons
 linkList.addEventListener('click', (e) => {
-   if (e.target.tagName === 'BUTTON') {
-      let pageNum = e.target.textContent
-      document.querySelectorAll('ul.link-list button')[activeButtonIndex].className = ''
-      e.target.className = 'active'
-      activeButtonIndex = pageNum - 1
-      showPage(data, pageNum)
-   }
+  if (e.target.tagName === 'BUTTON') {
+    let pageNum = e.target.textContent
+    document.querySelectorAll('ul.link-list button')[activeButtonIndex].className = ''
+    e.target.className = 'active'
+    activeButtonIndex = pageNum - 1
+    showPage(data, pageNum)
+  }
 })
 
-
-
-//-----------------------------------------------------------------------------
-//FUNCTION CALLS
-//-----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// FUNCTION CALLS
+// -----------------------------------------------------------------------------
 
 showPage(data, 1)
 addPagination(data)
 filterSearch(data)
-
